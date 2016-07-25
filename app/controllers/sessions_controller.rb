93ed3c13805660903afba7592e4ca86b4a6ab678
Babin Dahal <babin.dahal3@gmail.com>
2016-07-25 22:18:58 +0545

    fancy box added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
  
   def create
    user = User.from_omniauth(request.env["omniauth.auth"])
-  	# user = User.new(user_params)
+  	
      session[:user_id] = user.id
     redirect_to root_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,11 @@ match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 match 'auth/failure', to: redirect('/'), via: [:get, :post]
 match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
-# get 'auth/:provider/callback', to: 'users#create'
-# # match 'auth/:provider/callback', to: 'sessions#create'
-
-# get 'auth/failure', to: redirect('/')
-# get 'signout', to: 'sessions#destroy', as: 'signout'
 
  root 'posts#index'
 resources :posts
   resources :posts do 
   	resources :comments
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
