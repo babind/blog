5e2581c7d461543846a07d297da4270852ab9dec
Babin Dahal <babin.dahal3@gmail.com>
2016-07-26 02:15:34 +0545

    new change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-# match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-	get  'auth/:provider/callback', to: 'sessions#create'
+match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+	# get  'auth/:provider/callback', to: 'sessions#create'
 match 'auth/failure', to: redirect('/'), via: [:get, :post]
 match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
