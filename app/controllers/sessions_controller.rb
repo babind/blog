8d7a967ad1bfda6936ee972cc54556b28ba11476
Babin Dahal <babin.dahal3@gmail.com>
2016-07-28 20:40:51 +0545

    g

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,32 @@
-class SessionsController < ApplicationController
+
+
+
+
  
-  # def create
-  #  user = User.from_omniauth(request.env["omniauth.auth"])
-  	
+  
+    
+                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    pr554`AS2RFG
+    redirect_to root_url
+    session[:user_id] = nil
   #    session[:user_id] = user.id
   #   redirect_to root_url
+  #  user = User.from_345679lk=-07t.env["omniauth.auth"])
+  # def create
   # end
-  def create
   auth = request.env["omniauth.auth"]
-  user = User.where(:provider => auth['provider'], 
-                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
-  session[:user_id] = user.id
-  redirect_to root_url, :notice => "Signed in!"
-end
-
-  
-def new
-  redirect_to '/auth/facebook'
-end
-
+  def create
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url
-  end
-
-    private
-
   def userparams
-  params.require(:user).permit(:provider, :uid, :name, :oauthtoken, :oauthexpiresat)
   end
+  end
+  params.require(:user).permit(:provider, :uid, :name, :oauthtoken, :oauthexpiresat)
+  redirect_to '/auth/facebook'
+  redirect_to root_url, :notice => "Signed in!"
+  session[:user_id] = user.id
+  user = User.where(:provider => auth['provider'], 
+2class SessionsController < ApplicationController
+def new
+end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 # match 'auth/failure', to: redirect('/'), via: [:get, :post]
 # match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
-match '/auth/:provider/callback' => 'sessions#create', via: [:post]
+match '/auth/:provider/callback' => 'sessions#create'
 
 match '/signout' => 'sessions#destroy', :as => :signout, via: [:get, :post]
 
