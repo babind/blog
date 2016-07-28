0c33d9cdd1a1d6f554fd00220c190e54b0a89ef6
Babin Dahal <babin.dahal3@gmail.com>
2016-07-28 08:57:27 +0545

    r

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-		def self.create_with_omniauth(auth)
-			  create! do |user|
-			    user.provider = auth['provider']
-			    user.uid = auth['uid']
-			    if auth['info']
-			      user.name = auth['info']['name'] || ""
-			      user.email = auth['info']['email'] || ""
-			    end
-			end
+		def self.from_omniauth(auth)
+ +		  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+ +		    user.provider = auth.provider 
+ +		    user.uid      = auth.uid
+ +		    user.name     = auth.info.name
+ +		    user.save
+ +		  end
+ +	end
 
  end
 
